50774d522eebbe516f947dc7449f7f3a7f92e015
Puneet Rai <138113836+Puneetrai004@users.noreply.github.com>
2024-07-19 04:15:30 +0530

    Update_Fashion_MNIST.py

--- projects/1-fashion-mnist/train.py
+++ projects/1-fashion-mnist/train.py
@@ -8,34 +8,43 @@
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+!pip install wandb
 import wandb
 from wandb.keras import WandbCallback
 
 # logging code
-run = wandb.init(project="fashion")
-
+run = wandb.init(project="fashion_mnist_cnn")
 # load data
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
 img_width = X_train.shape[1]
 img_height = X_train.shape[2]
 
+
 # one hot encode outputs
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
+
 labels = ["T-shirt/top", "Trouser", "Pullover", "Dress",
           "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
-
+print(fashion_mnist.load_data())
 num_classes = y_train.shape[1]
+print(num_classes)
 
 # create model
 model = Sequential()
-model.add(Flatten(input_shape=(img_width, img_height)))
-model.add(Dense(num_classes))
+model.add(Flatten(input_shape = (img_width, img_height)))
+model.add(Dense(256, activation = "relu"))
+model.add(Dense(128, activation = "relu"))
+model.add(Dense(num_classes, activation = "softmax"))
 model.compile(loss='categorical_crossentropy', optimizer='adam',
               metrics=['accuracy'])
 
 # Fit the model
-model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test),
-          callbacks=[WandbCallback(data_type="image", labels=labels)])
+model.fit(X_train, y_train, epochs = 10, validation_data = (X_test, y_test), callbacks = [WandbCallback(data_type = "image", labels = labels)])
+
+test_loss, test_accu = model.evaluate(X_test, y_test)
+
+print("Test loss:", test_loss)
+print("Test accuracy:", test_accu)
